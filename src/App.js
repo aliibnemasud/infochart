b41cfadd412c4b1ae7ec315bc744dfe8eba70c07
Ali Ibne masud <mdmasudrony@gmail.com>
2022-09-17 21:07:53 +0600

    condition updated Yes and no working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-
 import './App.css';
-import Table from './componants/Table';
 import TableData from './componants/TableData';
 
 function App() {
--- src/Features/tableDataSlice.jsx
+++ src/Features/tableDataSlice.jsx
@@ -5,42 +5,42 @@ const allData = {
         {
             _id: "63246aeef42efbba4de685a9",
             isPaid: false,
-            amountPaid: "$2,316.52",
+            amountPaid: "No",
             name: "House Duran",
             email: "houseduran@interloo.com"
         },
         {
             _id: "63246aee8f8e1bc82e960e8b",
             isPaid: true,
-            amountPaid: "$2,837.26",
+            amountPaid: "Yes",
             name: "Ingrid Manning",
             email: "ingridmanning@interloo.com"
         },
         {
             _id: "63246aeefd38942619b5e115",
-            isPaid: true,
-            amountPaid: "$2,771.09",
+            isPaid: false,
+            amountPaid: "No",
             name: "Shelley Delacruz",
             email: "shelleydelacruz@interloo.com"
         },
         {
             _id: "63246aeea7fa84fe32965094",
             isPaid: true,
-            amountPaid: "$3,657.26",
+            amountPaid: "Yes",
             name: "Blackwell Velasquez",
             email: "blackwellvelasquez@interloo.com"
         },
         {
             _id: "63246aee2140e576374cb2e1",
             isPaid: true,
-            amountPaid: "$3,640.37",
+            amountPaid: "Yes",
             name: "Rhodes Moses",
             email: "rhodesmoses@interloo.com"
         },
         {
             _id: "63246aeefc2b91783ea55fd2",
             isPaid: false,
-            amountPaid: "$3,903.23",
+            amountPaid: "No",
             name: "Bolton Riddle",
             email: "boltonriddle@interloo.com"
         }
@@ -60,8 +60,8 @@ const tableDataSlice = createSlice({
             const index = state.userinfo.filter(info => info._id === id);
             if (index) {
                 index[0].isPaid = true;
+                index[0].amountPaid = "Yes";
             }
-
         },
     }
 })
